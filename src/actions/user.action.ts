4aa170c543836a79dbc2c966588b550f2baa7b27
mohdshuhaib <mohdshuhaib357@gmail.com>
2025-01-24 15:52:52 +0530

    added sidebar completely by ading profile info

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -32,3 +32,20 @@ export async function syncUser() {
     console.log("error in syncUser", error)
   }
 }
+
+export async function getUserByClerkId(clerkId: string) {
+  return prisma.user.findUnique({
+    where: {
+      clerkId
+    },
+    include: {
+      _count: {
+        select: {
+          followers: true,
+          following: true,
+          posts: true
+        }
+      }
+    }
+  })
+}
