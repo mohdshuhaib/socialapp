78cde29659adfd5de37d427a3779db70882c5dbe
mohdshuhaib <mohdshuhaib357@gmail.com>
2025-01-24 09:29:13 +0530

    added navbar completely

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>
-            Sign in
-          </Button>
-        </SignInButton>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+
     </div>
   );
 }
