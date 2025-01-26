afb75ea1e33994bd7f3b95c0acbb600be301a108
mohdshuhaib <mohdshuhaib357@gmail.com>
2025-01-26 07:37:17 +0530

    creating post and send to the database and impage ulpoad will do in next session

---
+++ src/actions/post.action.ts
@@ -0,0 +1,23 @@
+'use server'
+
+import prisma from "@/lib/prisma"
+import { getDbUserId } from "./user.action"
+import { revalidatePath } from "next/cache"
+
+export async function createPost(content: string, image: string) {
+  try {
+    const userId = await getDbUserId()
+    const post = await prisma.post.create({
+      data: {
+        content,
+        image,
+        authorId: userId
+      }
+    })
+    revalidatePath('/')
+    return {succes:true, post}
+  } catch (error) {
+    console.error('failed to create post:', error)
+    return {succes:false, error: 'failed to create post'}
+  }
+}
