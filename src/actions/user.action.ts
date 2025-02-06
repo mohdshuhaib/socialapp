82aa96ccf6f5051da6e5a02939a5c4de8472b72b
mohdshuhaib <mohdshuhaib357@gmail.com>
2025-02-06 15:19:14 +0530

    post upload set and notification set

--- src/actions/user.action.ts
+++ src/actions/user.action.ts
@@ -54,7 +54,7 @@ export async function getUserByClerkId(clerkId: string) {
 
 export async function getDbUserId() {
   const { userId: clerkId } = await auth()
-  if (!clerkId) throw new Error('User not authenticated')
+  if (!clerkId) return null
   const user = await getUserByClerkId(clerkId)
   if (!user) throw new Error('User not found')
   return user.id
@@ -63,6 +63,7 @@ export async function getDbUserId() {
 export async function getRandomUsers() {
   try {
     const userId = await getDbUserId()
+    if(!userId) return []
     const randomUsers = await prisma.user.findMany({
       where: {
         AND: [
